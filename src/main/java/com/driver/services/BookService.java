0c9d8a2a7442f2cd4cb2381016ccacef6629e1eb
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-01-15 21:52:46 +0530

    DONE

--- src/main/java/com/driver/services/BookService.java
+++ src/main/java/com/driver/services/BookService.java
@@ -16,32 +16,38 @@ public class BookService {
     @Autowired
     BookRepository bookRepository2;
 
-    @Autowired
-    AuthorRepository authorRepository1;
+//    @Autowired
+//    AuthorRepository authorRepository1;
 
     public void createBook(Book book){
 
-        int authorId = book.getAuthor().getId();
-        Author author = authorRepository1.findById(authorId).get();
-
-        author.getBooksWritten().add(book);
-        book.setAuthor(author);
+//        int authorId = book.getAuthor().getId();
+//        Author author = authorRepository1.findById(authorId).get();
+//
+//        author.getBooksWritten().add(book);
+//        book.setAuthor(author);
 
         bookRepository2.save(book);
-        authorRepository1.save(author);
+//        authorRepository1.save(author);
     }
 
     public List<Book> getBooks(String genre, boolean available, String author){
         List<Book> books = null; //find the elements of the list by yourself
 
-        if(genre != null && author != null){
-            books = bookRepository2.findBooksByGenreAuthor(genre, author, available);
-        } else if (genre != null) {
+//        if(genre != null && author != null){
+//            books = bookRepository2.findBooksByGenreAuthor(genre, author, available);
+//        } else if (genre != null) {
+//            books = bookRepository2.findBooksByGenre(genre, available);
+//        } else if (author != null) {
+//            books = bookRepository2.findBooksByAuthor(author, available);
+//        }else {
+//            books = bookRepository2.findByAvailability(available);
+//        }
+
+        if(author == null){
             books = bookRepository2.findBooksByGenre(genre, available);
-        } else if (author != null) {
+        }else{
             books = bookRepository2.findBooksByAuthor(author, available);
-        }else {
-            books = bookRepository2.findByAvailability(available);
         }
 
         return books;
